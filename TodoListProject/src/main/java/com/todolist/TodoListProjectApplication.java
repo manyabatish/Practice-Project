6ec1a43a744ceb89acdf4ec9fb5fb4f9e8e22f6b
Manya <manyabatish99@gmail.com>
2021-08-02 14:18:23 +0530

    Improvement

--- TodoListProject/src/main/java/com/todolist/TodoListProjectApplication.java
+++ TodoListProject/src/main/java/com/todolist/TodoListProjectApplication.java
@@ -2,12 +2,15 @@ package com.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = "com.todolist.*")
 public class TodoListProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoListProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListProjectApplication.class, args);
+    }
+
 
 }
